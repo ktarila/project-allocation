ae20708f78b14a648bb746b0e77e09d66ae0f9f5
Patrick Kenekayoro <ktarila@gmail.com>
2018-05-17 12:15:04 +0100

    implemented evaluation of solution quality -- changed generate random sol to choose from student preference

--- project_allocation.py
+++ project_allocation.py
@@ -12,16 +12,8 @@ def main():
     # print(PROJECT_AREAS)
     # print(SUPERVISORS)
     rand_solution = solution.create_random_solution()
-    # print(rand_solution)
-    # allocation = solution.solution_to_allocation(rand_solution)
-    # print(allocation)
-    # supervisors = solution.get_supervisor_students(allocation)
-    # for item in supervisors:
-    #     print(item, len(supervisors[item]))
-    lect_num_penalty = solution.get_extra_student_penalty(rand_solution)
-    stud_pref_penalty = solution.get_student_pref_penalty(rand_solution)
-    print(lect_num_penalty, stud_pref_penalty, (
-        lect_num_penalty + stud_pref_penalty))
+    quality = solution.get_solution_quality(rand_solution)
+    print("Quality of ", rand_solution, "is: ", quality)
 
 
 if __name__ == '__main__':
--- project_allocation/solution.py
+++ project_allocation/solution.py
@@ -2,24 +2,33 @@
 
 import itertools
 from operator import itemgetter
-import numpy as np
+from statistics import mean
+from random import choice
+# import numpy as np
 from project_allocation import config as cfg
 
 
 def create_random_solution():
     """Create a random solution to the project allocation problem"""
-    max_projects = len(cfg.PROJECT_AREAS)
-    max_students = len(cfg.STUDENTS)
-    return list(np.random.randint(1, max_projects, max_students))
+    # max_projects = len(cfg.PROJECT_AREAS)
+    # max_students = len(cfg.STUDENTS)
+    rand_sol = []
+    for value in cfg.STUDENTS:
+        # print(value)
+        rand_sol.append(choice(value['proj_pref']))
+    # print(len(rand_sol))
+    return rand_sol
+    # return list(np.random.randint(1, max_projects, max_students))
 
 
 def solution_to_allocation(solution):
     """Convert a solution to project allocation"""
     allocation_list = []
     for idx, val in enumerate(solution):
+        val_idx = val - 1  # convert index to 0 to n -1
         allocation_list.append(
             {'student': cfg.STUDENTS[idx]['name'], 'supervisor': cfg.PROJECT_AREAS[
-                val]['supervisor'], 'area': cfg.PROJECT_AREAS[val]['area']})
+                val_idx]['supervisor'], 'area': cfg.PROJECT_AREAS[val_idx]['area']})
     return allocation_list
 
 
@@ -66,3 +75,39 @@ def get_student_pref_penalty(solution):
         # print(pref_penalty, value, stud_prefs)
         penalty += pref_penalty
     return penalty
+
+
+def get_cgpa_equal_penalty(solution):
+    """Get sum of difference between Lecturers average cgpa"""
+    penalty = 0
+    allocation_list = solution_to_allocation(solution)
+    supervisor_students = get_supervisor_students(allocation_list)
+    mean_cgpa_per_supervisor = {}
+    for key, value in supervisor_students.items():
+        stud_details = []
+        for student in value:
+            stud = [float(item['cgpa'])
+                    for item in cfg.STUDENTS if item['name'] == student['student']]
+            stud_details = stud_details + stud
+        mean_cgpa_per_supervisor[key] = mean(stud_details)
+    # print(mean_cgpa_per_supervisor)
+    # get unique supervisor combination pairs
+    supervisor_combinations = list(itertools.combinations(cfg.SUPERVISORS, 2))
+    # difference in each pair
+    for item in supervisor_combinations:
+        val = list(item)
+        difference = abs(mean_cgpa_per_supervisor[
+            val[0]] - mean_cgpa_per_supervisor[val[1]])
+        penalty += difference
+    return penalty
+
+
+def get_solution_quality(solution):
+    """Get the quality of a timetable solution"""
+    gesp = get_extra_student_penalty(solution)
+    gspp = get_student_pref_penalty(solution)
+    gcep = get_cgpa_equal_penalty(solution)
+    print('GESP - Above average num students', gesp)
+    print('gcep - CGPA equal average', gcep)
+    print('gspp - Student Pref', gspp)
+    return gesp * 5 + 10 * gcep + gspp
